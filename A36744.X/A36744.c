a82080be7ce34c7d91530309ab061a5a492eea86
TheaHenry <thenry@etm-inc.com>
2015-06-19 10:21:24 -0700

    Initial release

---
+++ A36744.X/A36744.c
@@ -0,0 +1,281 @@
+#include "A36744.h"
+#include "FIRMWARE_VERSION.h"
+
+// This is the firmware for Interface Board
+
+
+_FOSC(ECIO & CSW_FSCM_OFF); 
+_FWDT(WDT_ON & WDTPSA_512 & WDTPSB_8);  // 8 Second watchdog timer 
+_FBORPOR(PWRT_OFF & BORV45 & PBOR_ON & MCLR_EN);
+_FBS(WR_PROTECT_BOOT_OFF & NO_BOOT_CODE & NO_BOOT_EEPROM & NO_BOOT_RAM);
+_FSS(WR_PROT_SEC_OFF & NO_SEC_CODE & NO_SEC_EEPROM & NO_SEC_RAM);
+_FGS(GWRP_OFF & GSS_OFF);
+_FICD(PGD);
+
+
+LTC265X U23_LTC2654;
+
+#define EK_VOLTAGE_TABLE_VALUES 50,76,102,128,153,179,204,229,253,277,300,322,344,366,386,406,425,443,460,476,491,505,517,529,540,549,557,564,570,574,577,579,580,579,577,574,570,564,557,549,540,529,517,505,491,476,460,443,425,406,386,366,344,322,300,277,253,229,204,179,153,128,102,76,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50
+
+#define TOP_VOLTAGE_TABLE_VALUES 50,65,79,94,109,123,137,151,165,178,191,204,217,229,240,251,262,272,282,291,299,307,315,321,327,332,337,341,344,347,349,350,350,350,349,347,344,341,337,332,327,321,315,307,299,291,282,272,262,251,240,229,217,204,191,178,165,151,137,123,109,94,79,65,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50
+
+
+const unsigned int EkReferenceVoltageTable[256] = {EK_VOLTAGE_TABLE_VALUES};  // This table defines the cathode reference voltage per the resistor value
+const unsigned int TopReferenceVoltageTable[128]  = {TOP_VOLTAGE_TABLE_VALUES};   // This table defines the top reference voltage per the resistor value
+
+
+ControlData global_data_A36744;
+
+void DoStateMachine(void);
+void InitializeA36744(void);
+int CheckHeaterFlt (void);
+
+#define STATE_STARTUP       0x10
+#define STATE_WARMUP	    0x20
+#define STATE_READY		    0x30
+#define STATE_SHUTDOWN      0x40
+
+int main(void) {
+  global_data_A36744.control_state = STATE_STARTUP;
+  while (1) {
+    DoStateMachine();
+  }
+}
+
+
+void DoStateMachine(void) {
+  switch (global_data_A36744.control_state) {
+
+  case STATE_STARTUP:
+    InitializeA36744();
+    global_data_A36744.heater_warmup_timer = HTR_WARMUP_DEFAULT_DURATION;
+	if SHORT_RESET_NOT == 0 //checking power interrupt duration
+	{
+		global_data_A36744.heater_warmup_timer = HTR_WARMUP_RECOVERY_DURATION;
+	}
+	else
+	{
+		Convert ADC Ek resistor
+		Convert ADC Top resistor
+		while (conversion incomplete)
+		Get Top and Ek reference voltages.
+		output ek and top voltages.
+		output htr voltage.
+		while (conversion incomplete)
+	}
+    global_data_A36744.control_state = STATE_WARMUP;
+    break;
+
+	
+  case STATE_WARMUP:
+    int flash_LED_timer = 10;
+	PIN_STANDBY = 1;
+	PIN_HTR_ENABLE_NOT = 0;
+	while (global_data_A36744.heater_warmup_timer > 0)
+	{
+		output heater set voltage.
+		if (_T3IF == 1)
+		{
+			_T3IF = 0;
+			global_data_A36744.heater_warmup_timer--;
+			flash_LED_timer--;
+		}
+		if (flash_LED_timer==0)
+		{
+			flash_LED_timer = 50;
+			PIN_HTR_LED =^ 1;
+		}
+		if (SHORT_HEAT == 1 && global_data_A36744.heater_set_voltage != HEATER_FAST_WARMUP_VOLTAGE) 
+		{
+			global_data_A36744.heater_set_voltage = HEATER_FAST_WARMUP_VOLTAGE;
+			
+			if global_data_A36744.heater_warmup_timer > HTR_WARMUP_SHORT_DURATION
+				global_data_A36744.heater_warmup_timer = HTR_WARMUP_SHORT_DURATION;
+		}		
+		if global_data_A36744.heater_warmup_timer <= HV_ON_DELAY
+			PIN_PIC_HV_ON = 1 ;
+		if (volterrn digital input goes low) {
+	global_data_A36744.control_state = STATE_SHUTDOWN;
+	}
+	if (CheckHeaterFlt)
+		PIN_HTR_LED = 1;
+	else
+		PIN_HTR_LED = 0;
+	
+	}
+    global_data_A36744.heater_set_voltage = HEATER_DEFAULT_VOLTAGE;
+	global_data_A36744.heater_warmup_timer = HTR_WARMUP_DEFAULT_DURATION;
+	PIN_STANDBY = 0;
+	PIN_ETM_RESET_DETECT = 1;
+	global_data_A36744.control_state = STATE_READY;
+    break;
+
+  case STATE_READY:
+  /* 
+  During the ready state, the controller checks for the following conditions:
+	1. Heater Backoff - if there is no pulse input (pretrans) for a given amount of time (heater backoff window), 
+		then the heater voltage will be backed off to heater backoff voltage.	
+		Implementation notes - pretrans is used as an input capture, which asserts its flag on every rising edge. As long as 
+		a rising edge is detected, the backoff counter is reset. If there is no rising edge for long enough, the counter will
+		reach zero and lower the heater voltage.
+	2. Arc fault - if a number of repeated arcs (arcs repeated) is detected within a given 
+		amount of time (arc fault window), then an arc fault is generated.
+	3. Check for Volterrn condition- once asserted, the power supply needs to be shut down. When cleared, 
+		the power supply needs to be brought up again and start the heater warmup sequence.
+  */
+  
+	_T3IF = 0;
+	//_IC1IF = 0;
+    while (global_data_A36744.control_state == STATE_READY) {
+		
+		//if _IC1IF == 1 // keep heater at its default values as long as pulses are coming in.
+		{
+		//	_IC1IF = 0;
+			global_data_A36744.heater_backoff_time_counter = HTR_BACKOFF_WINDOW;
+			global_data_A36744.heater_set_voltage = HEATER_DEFAULT_VOLTAGE;
+		}		
+			
+			
+      if (_T3IF == 1)
+		{
+			_T3IF = 0;
+			global_data_A36744.heater_backoff_time_counter--; 
+			if global_data_A36744.heater_backoff_time_counter == 0 // change heater voltage to backoff value if no pulses came in for x amount of time.
+			{
+				global_data_A36744.heater_set_voltage = HEATER_BACKOFF_VOLTAGE;
+			}
+		}
+	   output htr voltage.
+	  
+	  
+	  check arc detect
+	  if (volterrn digital input goes low) {
+	global_data_A36744.control_state = STATE_SHUTDOWN;
+	}
+	if (CheckHeaterFlt)
+		PIN_HTR_LED = 1;
+	else
+		PIN_HTR_LED = 0; 
+      
+    }
+    break;
+     
+    
+ case STATE_SHUTDOWN:
+    Disable HV
+	disable heater
+	wait 50ms
+	disable grid
+	
+	while (global_data_A36744.control_state == STATE_SHUTDOWN) {
+     	 if volterrn goes hi
+	 {
+		 turn on grid
+		 wait 10ms
+		 set heater voltage to default value.
+		 global_data_A36744.control_state = STATE_WARMUP;
+	 }
+	 
+    }
+	break;
+
+	
+  default:
+    global_data_A36744.control_state = STATE_READY;
+    break;
+
+  }
+}
+
+
+void InitializeA36744(void) {
+
+  TRISA = A36744_TRISA_VALUE;
+  TRISB = A36744_TRISB_VALUE;
+  TRISC = A36744_TRISC_VALUE;
+  TRISD = A36744_TRISD_VALUE;
+  TRISF = A36744_TRISF_VALUE;
+  TRISG = A36744_TRISG_VALUE;
+
+  PIN_PIC_ARC_FLT_NOT = 1;
+  PIN_HTR_ENABLE_NOT = 1;
+
+  PIN_STANDBY   = 0;
+  PIN_POR   = 0;
+  PIN_PIC_HV_ON   = 0;
+  PIN_GRID_ENABLE   = 1;
+  //PIN_ETM_RESET_DETECT
+  PIN_OVERLOAD = 0;
+  PIN_PIC_ERROR = 0;
+  PIN_HTR_LED = 0;
+  PIN_PIC_VOLTERRN_NOT = 1;
+  //PIN_TEST_POINT_A =0;
+  PIN_LED_OPERATIONAL_GREEN = 1;
+  //PIN_LED_A_RED = 0;
+
+
+  //Timer1 setup
+  PR1 = PR1_SETTING;
+  _T1IF = 0; 		// Clear Timer1 interrupt flag
+ //  _T1IP = 6; 	// Set Timer1 interrupt priority
+  _T1IE = 0; 		//Disable Timer1 Interrupt
+  T1CON = T1CON_SETTING;
+
+  //Timer3 setup
+  PR3 = PR3_VALUE_10_MILLISECONDS;
+  T3CON = T3CON_VALUE;
+  _T3IF = 0;
+  
+//ADC setup
+  ADCON2 = ADCON2_SETTING;
+  ADCON3 = ADCON3_SETTING;
+  ADCHS  = ADCHS_SETTING;
+  ADPCFG = ADPCFG_SETTING;
+  ADCSSL = ADCSSL_SETTING;
+  ADCON1 = ADCON1_SETTING;
+  
+// additional interrupt set-up
+//  _INT1IF = 0;
+//  _INT1IP = 7;
+//  _INT1IE = 1;
+
+  
+
+  // Initialize LTC DAC
+  //SetupLTC265X(&U23_LTC2654, ETM_SPI_PORT_1, FCY_CLK, LTC265X_SPI_2_5_M_BIT, _PIN_RG15, _PIN_RC1);
+  SetupLTC265X(&U23_LTC2654, ETM_SPI_PORT_1, FCY_CLK, LTC265X_SPI_2_5_M_BIT, _PIN_RC1, _PIN_RC3);
+
+//#define AFT_CONTROL_VOLTAGE_MAX_PROGRAM  12000
+//#define AFT_CONTROL_VOLTAGE_MIN_PROGRAM  1000
+
+  ETMAnalogInitializeOutput(&global_data_A36744.heater_set_voltage,
+			    MACRO_DEC_TO_SCALE_FACTOR_16(3.15),
+			    OFFSET_ZERO,
+			    ANALOG_OUTPUT_0,
+			    AFT_CONTROL_VOLTAGE_MAX_PROGRAM,
+			    AFT_CONTROL_VOLTAGE_MIN_PROGRAM,
+			    0);
+
+  ETMAnalogInitializeOutput(&global_data_A36744.aft_control_voltage,
+			    MACRO_DEC_TO_SCALE_FACTOR_16(3.98799),
+			    OFFSET_ZERO,
+			    ANALOG_OUTPUT_0,
+			    AFT_CONTROL_VOLTAGE_MAX_PROGRAM,
+			    AFT_CONTROL_VOLTAGE_MIN_PROGRAM,
+			    0);
+
+  ETMAnalogInitializeOutput(&global_data_A36744.aft_control_voltage,
+			    MACRO_DEC_TO_SCALE_FACTOR_16(3.98799),
+			    OFFSET_ZERO,
+			    ANALOG_OUTPUT_0,
+			    AFT_CONTROL_VOLTAGE_MAX_PROGRAM,
+			    AFT_CONTROL_VOLTAGE_MIN_PROGRAM,
+			    0);
+  
+}
+
+int CheckHeaterFlt (void) {
+	return (HTR_SUM_FLT || HTR_OC_FLT || HTR_UC_FLT);
+}
+
+
---
+++ A36744.X/A36744.h
@@ -0,0 +1,182 @@
+// Header file for new Interface Board
+#ifndef __A36744_H
+#define __A36744_H
+
+
+#define FCY_CLK 10000000
+
+
+#include <xc.h>
+#include <libpic30.h>
+#include <adc10.h>
+#include <timer.h>
+#include <pwm.h>
+
+#include "ETM.h"
+
+/*
+  Hardware Module Resource Usage
+
+  SPI1   - Used/Configured by LTC265X Module
+  Timer3 - 10ms timer 
+  Timer1 - Used for repeated arc detection
+
+*/
+
+
+// ----------------- IO PIN CONFIGURATION -------------------- //
+// All unused pins will be set to outputs and logic zero
+// LAT values default to 0 at startup so they do not need to be manually set
+
+// Pins to be configured as inputs
+/*
+
+  RA6 - Digital Input - Ek O/V Flt
+  RA7 - Digital Input - Collector U/V Flt
+  RA9 - VREF-
+  RA10 - VREF+
+  RA12 - Digital Input -  VOLTERRN
+  RA13 - INT2 - ARC DETECT
+ 
+  RB0  - ICD - PROGRAM
+  RB1  - ICD - PROGRAM
+  RB2 - Digital Input - Ek U/V Flt
+  RB3  - Analog Input - Ek Resistor Sense
+  RB4  - Analog Input - Top Resistor Sense
+  RB5 - Digital Input - Over PW Flt
+  RB7 - Analog Input - ETM ADC +15V monitor
+
+  RC1  - DAC LDAC  (Configured by DAC module)
+  RC4  - HV ON SENSE
+  RC14 - Digital Input - Heater Sum Flt
+  
+  RD1 - Digital Input - Short reset (short power cycle)
+  RD4 - Digital Input - Temp SW Flt
+  RD5 - Digital Input - Heater O/C Flt
+  RD6 - Digital Input - Low Line Flt
+  RD7 - Digital Input - Iw O/C Flt
+  RD8 - Digital Input - Pretrans
+  RD9 - Digital Input - Short heat
+  RD10 - Digital Input - Reset
+  RD13 - Digital Input - Inverter O/C Flt
+  RD15 - Digital Input - Grid U/V Flt
+  
+  RF2 - UART (not used)
+  RF3 - UART (not used)
+  RF4 - Digital Input - Grid O/C Flt
+  RF5 - Digital Input - Heater U/C Flt
+  RF6 - SPI1 (Configured by Pic Module)
+  RF7 - SPI1 (Configured by Pic Module)
+  RF8 - SPI1 (Configured by Pic Module)
+  
+  RG6 - Digital Input - TWT over temp Flt
+  RG7 - Digital Input - PS over temp Flt
+  RG8 - Digital Input - Grid O/V Flt
+  RG9 - Digital Input - Over Duty Flt
+  RG15  - DAC CS/LD (Configured by DAC module)
+
+*/
+
+
+#define A36744_TRISA_VALUE 0b0011011011000000 
+#define A36744_TRISB_VALUE 0b0000000010111111 
+#define A36744_TRISC_VALUE 0b0100000000010010 
+#define A36744_TRISD_VALUE 0b1010011111110010
+#define A36744_TRISF_VALUE 0b0000000111111100 
+#define A36744_TRISG_VALUE 0b1000001111000000
+
+
+// ------------- PIN DEFINITIONS ------------------- ///
+
+//Control
+#define PIN_PIC_ARC_FLT_NOT         	_LATB10
+#define PIN_HTR_ENABLE_NOT           	_LATD11
+#define PIN_STANDBY			            _LATD0
+#define PIN_POR				            _LATD2
+#define PIN_PIC_HV_ON				    _LATG0
+#define PIN_GRID_ENABLE				    _LATG3
+#define PIN_ETM_RESET_DETECT		    _LATG14
+
+//Status
+#define PIN_OVERLOAD          			_LATC2
+#define PIN_PIC_ERROR        		    _LATB14
+#define PIN_HTR_LED				    	_LATG12
+#define PIN_PIC_VOLTERRN_NOT			_LATB13
+
+// Test Points
+#define PIN_TEST_POINT_A                 _LATB11
+
+// LEDs
+#define PIN_LED_OPERATIONAL_GREEN        _LATA14
+#define PIN_LED_A_RED                    _LATG8
+
+//Discrete Inputs
+#define SHORT_RESET_NOT					_RD1
+#define SHORT_HEAT						_RD9
+#define HTR_SUM_FLT						_RC14
+#define HTR_OC_FLT						_RD5
+#define HTR_UC_FLT						_RF5
+#define PRETRANS						_RD8
+
+
+// ---------------- Timing Configuration Values ------------- //
+#define ARC_FLT_WINDOW	        1000        // 3 arcs within defined time will assert an arc fault
+#define HTR_BACKOFF_WINDOW      5400          // Duration with no pulse to start heater backoff
+#define HTR_WARMUP_DEFAULT_DURATION     18000           // Default duration for heater timer delay
+#define HV_ON_DELAY             500      // Time allotted for HV turn on during warmup
+#define HTR_WARMUP_SHORT_DURATION     6000           // Fast warmup duration in secs for heater timer delay
+#define HTR_WARMUP_RECOVERY_DURATION	1000
+
+#define HEATER_DEFAULT_VOLTAGE		3.15 	// 6.3V
+#define HEATER_FAST_WARMUP_VOLTAGE	3.6		// 7V
+#define HEATER_BACKOFF_VOLTAGE		3.075	//6.15V
+#define ARCS_REPEATED				5 
+
+
+// --------------------- T1 Configuration -----
+// With 1:8 prescale 
+
+#define T1CON_SETTING     (T1_ON & T1_IDLE_CON & T1_GATE_OFF & T1_PS_1_256 & T1_SYNC_EXT_OFF & T1_SOURCE_INT)
+#define PR1_SETTING  (unsigned int)(FCY_CLK / 32 / 8 / ARC_FLT_WINDOW)
+
+
+/* 
+   TMR3 Configuration
+   Timer3 - Used for 10msTicToc
+   Period should be set to 10mS
+   With 10Mhz Clock, x8 multiplier will yield max period of 17.7mS, 2.71uS per tick
+*/
+
+#define T3CON_VALUE                    (T3_ON & T3_IDLE_CON & T3_GATE_OFF & T3_PS_1_8 & T3_SOURCE_INT)
+#define PR3_PERIOD_US                  10000   // 10mS
+#define PR3_VALUE_10_MILLISECONDS      (unsigned int)((FCY_CLK / 1000000)*PR3_PERIOD_US/8)
+
+
+
+
+// -------------------  ADC CONFIGURATION ----------------- //
+//Still needs to be defined for application
+#define ADCON1_SETTING   (ADC_MODULE_ON & ADC_IDLE_STOP & ADC_FORMAT_INTG & ADC_CLK_MANUAL & ADC_SAMPLE_SIMULTANEOUS & ADC_AUTO_SAMPLING_ON)
+#define ADCON2_SETTING   (ADC_VREF_EXT_EXT & ADC_SCAN_OFF & ADC_CONVERT_CH_0ABC & ADC_SAMPLES_PER_INT_1 & ADC_ALT_BUF_ON & ADC_ALT_INPUT_OFF)
+#define ADCON3_SETTING   (ADC_SAMPLE_TIME_10 & ADC_CONV_CLK_SYSTEM & ADC_CONV_CLK_2Tcy)
+#define ADCHS_SETTING    (ADC_CHX_POS_SAMPLEA_AN3AN4AN5 & ADC_CHX_NEG_SAMPLEA_VREFN & ADC_CH0_POS_SAMPLEA_AN8 & ADC_CH0_NEG_SAMPLEA_VREFN & ADC_CHX_POS_SAMPLEB_AN3AN4AN5 & ADC_CHX_NEG_SAMPLEB_VREFN & ADC_CH0_POS_SAMPLEB_AN8 & ADC_CH0_NEG_SAMPLEB_VREFN)
+#define ADPCFG_SETTING   (ENABLE_AN2_ANA & ENABLE_AN3_ANA & ENABLE_AN4_ANA & ENABLE_AN5_ANA & ENABLE_AN8_ANA)
+#define ADCSSL_SETTING   (SKIP_SCAN_AN0 & SKIP_SCAN_AN1 & SKIP_SCAN_AN2 & SKIP_SCAN_AN6 & SKIP_SCAN_AN7 & SKIP_SCAN_AN9 & SKIP_SCAN_AN10 & SKIP_SCAN_AN11 & SKIP_SCAN_AN12 & SKIP_SCAN_AN13 & SKIP_SCAN_AN14 & SKIP_SCAN_AN15)
+
+typedef struct {
+  unsigned int control_state;
+  unsigned int heater_set_voltage;
+  unsigned int cathode_resistor;
+  unsigned int top_resistor;
+  unsigned int cathode_set_voltage;
+  unsigned int top_set_voltage;
+  unsigned int arc_counter;
+  unsigned int heater_warmup_timer;
+  unsigned int heater_backoff_time_counter;
+  unsigned int volterrn_time_counter;
+
+} ControlData;
+
+extern ControlData global_data_A36744;
+
+#endif
